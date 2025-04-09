ff8610787c4c8a00b6d88b5e89c0c5a0f8c9b467
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-10 00:42:09 +0600

    Delete: delete and element from an array using filter

---
+++ Lecture-05/delete-array-element.js
@@ -0,0 +1,14 @@
+const arr = [
+  { id: 1, value: 10 },
+  { id: 2, value: 20 },
+  { id: 3, value: 30 },
+  { id: 4, value: 40 },
+  { id: 5, value: 50 },
+];
+
+
+
+// filter - Immutable
+// using filter delete object from array
+// const withOutIdThreeItemArray = arr.filter(item => item.id != 3);
+// console.log(withOutIdThreeItemArray);
