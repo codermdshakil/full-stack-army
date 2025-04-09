d16dbc18a447c11d40518447670d36b7b7a47e30
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-09 15:58:37 +0600

    Learn : Statement and Expression

---
+++ Lecture-04/statement_expression.js
@@ -0,0 +1,16 @@
+
+// 1. statement is like a complete english sentence
+let a = 10;  // full complete sentence
+// console.log(a);
+
+// 2. An expression is like a math formula — it produces a value.
+
+const sum = 5 + 10; // here [5 + 10 is a expression] 
+
+
+// Analogy
+// Statement	Full sentence	    let a = 5;	Do this (save value in a)
+// Expression	A word or a value	5 + 3	    This gives 8 
+
+// Expression can be inside a statement! 
+// let sum = 5 + 3; 
