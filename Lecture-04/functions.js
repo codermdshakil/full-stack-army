cef60bc1c8a2bfcb5854734a0e2d355c82798df6
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-09 15:44:58 +0600

    Learn : functions and types of function and differents between function and loops

---
+++ Lecture-04/functions.js
@@ -0,0 +1,33 @@
+// 1. Function Declaration (also called Named Function)
+// function great() {
+//     console.log('Oi aiya porse Function bole mechine!');
+// }
+// great();
+
+// 2. Function Expression
+// const sayHello = function(){
+//     console.log('Ami hello koitase tumi hi kow aida hoitase function expression');
+// }
+// sayHello();
+
+// 3.Arrow Function (ES6, short and modern)
+// const sum = (a, b) => {
+//     return a + b;
+// }
+
+// console.log(sum(10, 20));
+
+// 5. Callback Function - call function inside a function
+
+// function greetUser(name, callback) {
+//   callback(name);
+// }
+
+// greetUser("Arafat", function (name) {
+//   console.log("Hello, " + name);
+// });
+
+// differents between function vs loop 
+// 1. when loopint start we don't have any control on it on the otherhand function have control 
+// 2. function is a resuable task otherhand loop iterate tasks
+// 3. function as name so that we can reuse it otherhand loop don't have any name to reuse it
