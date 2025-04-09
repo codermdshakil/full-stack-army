abfcdd0e77866a759dd96116817d0e670d3d680c
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-09 23:01:06 +0600

    Learn : using loop traverse array elements its called Imparative traverse

---
+++ Lecture-05/imparative-traverse-array.js
@@ -0,0 +1,15 @@
+
+const numbers = [1,6,3,6,8,3,7,9,4];
+
+// when we traverse using loops its called imparative traverse
+
+let sum = 0;
+for(let i = 0; i < numbers.length;i++){
+    sum += numbers[i];
+    console.log(numbers[i]);
+}
+console.log('sum = ', sum);
+
+
+
+
