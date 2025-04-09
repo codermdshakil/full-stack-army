91f7c5e8c9e69bd827b2e7a9207abb143d6810b1
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-09 14:13:32 +0600

    Learn : Assignment and Arithmetic operators

---
+++ Lecture-04/operators.js
@@ -0,0 +1,42 @@
+// operators are function
+
+// operators are mathmetical representation
+
+// Operators are 4 types
+
+// 1. Assignment Operators - asigns value to a variable.
+const x = 5;
+
+// 2. Arithmetic Operators -Used for calculations.
+
+
+// Operator	    Meaning	        Example	    Result
+// --------------------------------------------------
+// +	        Addition	    2 + 3	    5
+// -	        Subtraction	    5 - 2	    3
+// *	        Multiplication	4 * 2	    8
+// /	        Division	    10 / 2	    5
+// %	        Modulus	        10 % 3	    1
+// **	        Exponentiation	2 ** 3	    8
+// ++	        Increment	    x++	x       = x + 1
+// --	        Decrement	    x--	x       = x - 1
+
+const r1 = 5 + 10;
+const r2 = 51 - 10;
+const r3 = 5 * 10;
+const r4 = 10 /2;
+const r5 = 50 & 10;
+const r6 = 	2 ** 3;
+r1++;
+r5--;
+
+
+// 3. Comparison Operators - Used to compare values (returns true or false).
+// 4. Logical Operators - Used with boolean (true or false).
+// 5. Ternary Operator - Shorthand for if...else.
+// 6. Typeof Operator  - Checks the data type.
+
+
+
+
+
