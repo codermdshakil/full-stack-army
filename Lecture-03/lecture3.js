494808ade4f8dfb20f858dc6b2f474a48daa9276
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-09 13:20:17 +0600

    Lecture 03 : Programming fundamental lanscape start at 09/04/2025

---
+++ Lecture-03/lecture3.js
@@ -0,0 +1,36 @@
+// ### Ajenda
+
+// - Programming language landscape
+// - Visualize Programing syntax
+
+// ## Components of Programing Language
+
+// - Input (STD I)
+// - Processing (somehow)
+// - Output (STD O)
+
+// ## Processing 2 types
+
+// - **Foundational**
+//     - variable
+//     - statement / expression
+//     - operators
+//     - Logic Conditionals
+//     - Function
+//     - Array
+//     - Key-value pair data structure (object)
+    
+     
+    
+// - **Structural**
+//     - OOP - theory
+//     - Functional - concept
+//     - Design Pattern - using oop we make some patten
+//     - Design Principles - solid, dry ,kiss
+//     - DSA - (Not depend on programming language) we store data in a structure where we can easily access data and use them
+
+// Visual Programming Language : https://scratch.mit.edu/
+
+// - Upwork huge kaj ase
+// - Strapy with 1-2 headless CMS headless CMS, contentfull
+// - Don’t use multiple skill upwork main focused one
