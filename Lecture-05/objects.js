8753250c55ef108f6322db0367ca16a7dcc7ea33
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-10 22:49:35 +0600

    Learn : Create objects 2 ways and parts of object Noun/Adjective, Verb that means method

---
+++ Lecture-05/objects.js
@@ -0,0 +1,38 @@
+
+const microphone = {
+    brand:'Fifine',
+    indicator:true,
+    price:8000,
+    color:'Black',
+    startRecording:() => {
+       console.log('Start Recording!');
+    },
+    stopRecording:() => {
+       console.log('Stop Recording!!');
+    }
+}
+
+
+// Method
+// microphone.startRecording() 
+
+// Function
+// function start(){
+//     console.log('start');
+// }
+
+
+// There are two different part's of a object
+
+// 1. Noun / Adjective (state/data/property.field)
+// 2. Verb -> kaj : Method (Functionalities -> start, stop)
+
+
+
+// Create object using Object
+// console.log(microphone.toString());
+const testObj = new Object();
+testObj.name = "Shakil Ahmed"
+// console.log(testObj);
+
+
