1a9afa9c818d2fa77645ad050faf03bf4d3dc34a
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-10 21:13:32 +0600

    Learn : Copy element using slice

--- Lecture-05/copy-array-elements.js
+++ Lecture-05/copy-array-elements.js
@@ -5,6 +5,10 @@ const numbers = [1,2,3,4,5,6,7,8,9,10];
 const copyNumbers = [...numbers];
 // console.log(copyNumbers);
 
+// 2 way to copy array
+const copySecond = numbers.slice();
+console.log(copySecond);
+
 
 
 
