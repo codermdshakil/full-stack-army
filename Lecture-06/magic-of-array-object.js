3635bbf213c14a35bd178f6c55a667e4b26a5280
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-11 23:55:03 +0600

    Add element in the fast array and object : here also win Object so object is so fast

--- Lecture-06/magic-of-array-object.js
+++ Lecture-06/magic-of-array-object.js
@@ -13,23 +13,41 @@ for(let i = 0; i< 5000000; i++){
 
 
 // array  element finding time - 
-console.time('arra')
-const id = 4999999;
-const obj = arr.find(item => item.id === id);
-obj.value = '555';
-console.timeEnd('arra')
+// console.time('arra')
+// const id = 4999999;
+// const obj = arr.find(item => item.id === id);
+// obj.value = '555';
+// console.timeEnd('arra')
+
+console.time('array-first-element-added')
+// using unshift added value in fast of array
+arr.unshift({
+    id:5000000,
+    value:5000000
+});
+console.timeEnd('array-first-element-added')
 
 // object element finding time
 
-console.time('object')
-const id2 = 4999999;
-const obj2 = arrayToObject[id2];
-obj2.value = 'changed';
-console.timeEnd('object')
+// console.time('object')
+// const id2 = 4999999;
+// const obj2 = arrayToObject[id2];
+// obj2.value = 'changed';
+// console.timeEnd('object')
 
 
-// Time Differents
+console.time('Add-element-in-fast-of-object : ')
+arrayToObject[0] = {id:5000000, value:5000000};
+console.timeEnd('Add-element-in-fast-of-object : ')
+
+
+// Time Differents for add element in last of array and object
 // ---------------------- 
 // arra: 111.649ms
 // object: 0.027ms 
  
+
+// Time Differents for add element in first of array and object
+// -------------------------------------------------------------
+// array-first-element-added: 21.418ms
+// Add-element-in-fast-of-object : : 0.016ms
