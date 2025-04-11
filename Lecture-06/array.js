dfb3291f5b7567e404ff7a0068b81174ab3ac416
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-11 16:45:31 +0600

    Traverse array and object and array methods

--- Lecture-06/array.js
+++ Lecture-06/array.js
@@ -34,8 +34,6 @@ const students = [
   },
 ];
 
-
-
 /**
  * what can we do in Array
  * - Create a new one (easy)
@@ -48,43 +46,67 @@ const students = [
 
 // 1 Create a new person
 students.push({
-	id: '0a2c956c-a9f4-48b9-83fa-551b432dfb2b',
-	name: 'Fahim Faisal',
-	email: 'fahim@test.com',
+  id: "0a2c956c-a9f4-48b9-83fa-551b432dfb2b",
+  name: "Fahim Faisal",
+  email: "fahim@test.com",
 });
 
+// 2. Update
 
-// 2. Update 
-
-const idToUpdate = '63ecd4d0-2a92-41a5-acbb-cd54450f66dc';
+const idToUpdate = "63ecd4d0-2a92-41a5-acbb-cd54450f66dc";
 const updateData = {
-    name: 'Habiba Akhtar',
-	email: 'habiba@test.com',
-}
+  name: "Habiba Akhtar",
+  email: "habiba@test.com",
+};
 
 // 1 way to update not best practice
 // const updatedObj = students.find((student) => student.id === id );
 // updatedObj.name = updateData.name;
 // updatedObj.email = updateData.email;
 
-
-
 // 2 way to update  Best way
 const updatedIndex = students.findIndex((student) => student.id === idToUpdate);
 
-students[updatedIndex]={
-    ...students[updatedIndex], // er mane age ja data silo ta thakbe new jai data ashbe seti e just update hobe
-    ...updateData
-}
+students[updatedIndex] = {
+  ...students[updatedIndex], // er mane age ja data silo ta thakbe new jai data ashbe seti e just update hobe
+  ...updateData,
+};
 
 // console.log(students[updatedIndex]);
-console.log('Updated: ',students);
+// console.log('Updated: ',students);
 
-
-// 3 Delete 
+// 3 Delete
 students.splice(updatedIndex, 1);
-console.log('Delete: ', students);
+// console.log('Delete: ', students);
+
+// 4 Traverse
+
+// forEach, find, findIndex, filter, map, reduce, some, every
+
+// - forEach just performs side effects.
+// - map stores the transformed value.
+// - filter stores only the values that pass.
+// - reduce builds up a single return value.
+
+// students.forEach(student => console.log(student.name))
+// const result =  students.map(student => student.name + 'Yes');  // [ 'Md Al-AminYes', 'Akib AhmedYes', 'Fahim FaisalYes' ]
+
+// ** for of loop --> traverse array
+
+// for(const student of students){
+//     console.log(student);
+// }
+
+// ** for in loop --> traverse object
+const testObj = {
+  id: "63ecd4d30-2a92-41a5-acbb-cd54450f66dc",
+  name: "Test",
+  email: "test@test.com",
+};
+
+// for (const item in testObj) {
+//   //   console.log(item); // item just return property name of object
+// //   console.log(testObj[item]); // return value of object
+// }
 
 
-
- 
