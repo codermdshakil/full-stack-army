68dc07c9722bf63a3a1f52f965a1768983f58155
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-12 12:40:23 +0600

    Map, Filter

---
+++ Lecture-06/map-filter-reduce.js
@@ -0,0 +1,13 @@
+
+// const numbers = [1,2,3,4, false, '',NaN, 5, 6];
+
+// const filteredNumber = numbers.filter((v) => v);
+// const strs = filteredNumber.map((v) => v.toString());
+
+// shortcut // here save memory
+// const strs = numbers.filter((v) => v).map((v) => v.toString())
+// console.log(strs);
+
+
+
+
