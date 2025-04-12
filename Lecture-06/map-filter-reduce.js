6f87dbf5e7da0155ce64c553fd74e553a4bce575
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-12 12:56:04 +0600

    Reduce power

--- Lecture-06/map-filter-reduce.js
+++ Lecture-06/map-filter-reduce.js
@@ -9,5 +9,29 @@
 // console.log(strs);
 
 
+// Reduce
+
+// const numbers = [1,2,3,4,5,6,7];
+// const sum = numbers.reduce((a,b) => a+ b);
+// console.log(sum);
+
+
+
+const numbers = [1,2,3,4, false, '',NaN, 5, 6];
+
+// const result = numbers.reduce((acc, cur, idx) => {
+//     if(idx == 0) {
+//         acc+= '['
+//     }
+//     if(cur){
+//         acc += cur.toString() + (idx < numbers.length - 1 ? ', ': '');
+//     }
+//     if(idx == numbers.length-1) {
+//         acc+= ']'
+//     }
+//     return acc;
+// }, '');
+
+// console.log(result);  // [1,2,3,4,5,6];
 
 
